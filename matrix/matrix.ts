6ba1db5192ee002a414c6a6ac1a598074a8f88a9
Diogo Fernandes <diogo.fernandes@zoi.tech>
2023-10-19 13:15:21 +0200

    feat(matrix): matrix improved version 2.0

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,11 +1,19 @@
 export class Matrix {
   matrix: number[][];
-  rows: number[][];
-  columns: number[][];
+  matrixString: string;
 
   constructor(matrixString: string) {
-    this.rows = matrixString.split(/\n/).map(row => row.split(/\s+/).map(Number));
-    this.matrix = this.rows.slice();
-    this.columns = this.matrix[0].map((_, i) => this.matrix.map(row => row[i]));
+    this.matrixString = matrixString;
+    this.matrix = this.rows;
+  }
+
+  get rows(): number[][] {
+    return this.matrixString
+      .split(/\n/)
+      .map((row) => row.split(/\s+/).map(Number));
+  }
+
+  get columns(): number[][] {
+    return this.matrix[0].map((_, i) => this.matrix.map((row) => row[i]));
   }
 }
